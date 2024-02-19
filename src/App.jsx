968f671130570d4bd253a37e30988d66bf179de9
Cenzino21 <vincenzolabella2018@gmail.com>
2024-02-19 16:52:56 +0100

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
 import './App.css';
-import { useState } from 'react';
+//import { useState } from 'react';
 
-function AggiungiNota() {
+
+/*function Tabella() {
   const[numeroNota, setNumeroNota] = useState(0);
-  return(
-    <button onClick={() => setNumeroNota(numeroNota + 1)}>Aggiungi nota</button>
-  );
-}
-
-function Tabella() {
+  const[testoNota, setTestoNota] = useState("testo");
   return(
     <div id="NuovaNota">
-      <AggiungiNota />
+      <div>
+      <button onClick={() => setNumeroNota(numeroNota + 1) && setTestoNota("testo")}>Aggiungi nota</button> <br/>
+      <p>{numeroNota} {testoNota}</p>
+      </div>    
     </div>
   );
-}
+}*/
 
 function App() {
   return (
     <div className="App">
-      <p>Ciao Bro, ecco qua il tuo blocco note!!!</p>
-      <Tabella />
+      <h2 id="messaggioDiBenvenuto">Ciao Bro, ecco qua il tuo blocco note!!!</h2> <br/>
+      <div class="contenitore">
+        <div id="parteNote">
+          <h3>Elenco note</h3>
+          <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ol>
+        </div>
+        <div id="parteTesto">
+          <h3>Elenco testi</h3>
+          <input type="text" class="inserimentoTesto"></input> <br/>
+          <input type="text" class="inserimentoTesto"></input> <br/>
+          <input type="text" class="inserimentoTesto"></input> <br/>
+          <input type="text" class="inserimentoTesto"></input> <br/>
+          <input type="text" class="inserimentoTesto"></input>
+        </div>
+        </div>
     </div>
   );
 }
