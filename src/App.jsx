4d531a0e5ffb9096f640934e7fae8931c768190e
Cenzino21 <vincenzolabella2018@gmail.com>
2024-02-20 14:54:18 +0100

    Commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,44 @@
 import './App.css';
-//import { useState } from 'react';
-
-
-/*function Tabella() {
-  const[numeroNota, setNumeroNota] = useState(0);
-  const[testoNota, setTestoNota] = useState("testo");
-  return(
-    <div id="NuovaNota">
-      <div>
-      <button onClick={() => setNumeroNota(numeroNota + 1) && setTestoNota("testo")}>Aggiungi nota</button> <br/>
-      <p>{numeroNota} {testoNota}</p>
-      </div>    
-    </div>
-  );
-}*/
+import React from 'react';
 
 function App() {
+  /*
+  function eliminaNota() {
+    let indice = document.querySelector("#parteNote li");
+    let testo = document.querySelector("#parteTesto input[type='text']");
+    let bottone = document.querySelector("#parteTesto button");
+    if(indice && testo && bottone) {
+      indice.remove();
+      testo.remove();
+      bottone.remove();
+    }
+  }*/
   return (
     <div className="App">
-      <h2 id="messaggioDiBenvenuto">Ciao Bro, ecco qua il tuo blocco note!!!</h2> <br/>
+      <header id="messaggioDiBenvenuto"><h2>Ciao Bro, ecco qua il tuo blocco note!!!</h2></header>
       <div class="contenitore">
         <div id="parteNote">
           <h3>Elenco note</h3>
           <ol>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li class="numeriDelleNote"></li>
+            <li class="numeriDelleNote"></li>
+            <li class="numeriDelleNote"></li>
+            <li class="numeriDelleNote"></li>
+            <li class="numeriDelleNote"></li>
           </ol>
         </div>
         <div id="parteTesto">
           <h3>Elenco testi</h3>
-          <input type="text" class="inserimentoTesto"></input> <br/>
-          <input type="text" class="inserimentoTesto"></input> <br/>
-          <input type="text" class="inserimentoTesto"></input> <br/>
-          <input type="text" class="inserimentoTesto"></input> <br/>
+          <input type="text" class="inserimentoTesto"></input> 
+          <button type="button">Elimina</button> <br/>
+          <input type="text" class="inserimentoTesto"></input> 
+          <button type="button">Elimina</button> <br/>
           <input type="text" class="inserimentoTesto"></input>
+          <button type="button">Elimina</button> <br/>
+          <input type="text" class="inserimentoTesto"></input>
+          <button type="button">Elimina</button> <br/>
+          <input type="text" class="inserimentoTesto"></input>
+          <button type="button">Elimina</button> <br/>
         </div>
         </div>
     </div>
