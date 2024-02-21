909ed137ab227e509d11d99f0059a53b3b6bc2a5
Cenzino21 <vincenzolabella2018@gmail.com>
2024-02-21 17:37:52 +0100

    Commit 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import './App.css';
 import React from 'react';
 
-function App() {
-  /*
-  function eliminaNota() {
-    let indice = document.querySelector("#parteNote li");
-    let testo = document.querySelector("#parteTesto input[type='text']");
-    let bottone = document.querySelector("#parteTesto button");
-    if(indice && testo && bottone) {
-      indice.remove();
-      testo.remove();
-      bottone.remove();
-    }
-  }*/
+//Header della pagina
+function Intestazione() {
   return (
-    <div className="App">
-      <header id="messaggioDiBenvenuto"><h2>Ciao Bro, ecco qua il tuo blocco note!!!</h2></header>
-      <div class="contenitore">
-        <div id="parteNote">
-          <h3>Elenco note</h3>
+    <header id="messaggioDiBenvenuto"><h2>Ciao Bro, ecco qua il tuo blocco note!!!</h2></header>
+  )
+}
+
+//Titolo della parte delle note
+function Titolo() {
+  return (
+    <h3>Elenco note</h3>
+  )
+}
+
+//Elenco delle note
+function ElencoNote() {
+  return(
+    <div id="parteNote">
+          <Titolo />
           <button type="button">Aggiungi nota</button>
           <div>
             <p>Nota 1</p>
@@ -26,10 +27,27 @@ function App() {
             <p>Nota 3</p>
           </div>
         </div>
-        <div id="parteTesto">
-          <input type="text" placeholder="Titolo" id="inserimentoTitolo"></input>
-          <input type="text" placeholder="Testo" id="InserimentoTesto"></input>
-        </div>
+  )
+}
+
+//Parte testuale del blocco note
+function CompilaNota() {
+  return (
+    <div id="parteTesto">
+      <input type="text" placeholder="Titolo" id="inserimentoTitolo"></input>
+      <input type="text" placeholder="Testo" id="inserimentoTesto"></input>
+    </div>
+  )
+}
+
+//Funzione principale
+function App() {
+  return (
+    <div className="App">
+      <Intestazione />
+      <div class="contenitore">
+        <ElencoNote />
+        <CompilaNota />
       </div>
     </div>
   );
